9bc16b28c2a006e5f875e9242df6bb43ce3fb8d1
Víctor Colombo <victorcora98@gmail.com>
2022-03-23 21:45:18 -0300

    Remove bit number from FpscrBit struct

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,6 @@ mod fpscr {
     }
 
     pub struct FpscrBit {
-        number: BitNumber,
         name: String,
         is_set: bool,
         reserved: bool,
@@ -31,21 +30,14 @@ mod fpscr {
     impl FpscrBit {
         pub const fn dummy() -> Self {
             FpscrBit {
-                number: BitNumber::new(0),
                 name: String::new(),
                 is_set: false,
                 reserved: true,
             }
         }
 
-        pub fn new(
-            bit_number: u8,
-            name: &str,
-            is_set: bool,
-            reserved: bool,
-        ) -> Self {
+        pub fn new(name: &str, is_set: bool, reserved: bool) -> Self {
             FpscrBit {
-                number: BitNumber::new(bit_number),
                 name: name.to_string(),
                 is_set,
                 reserved,
@@ -64,205 +56,121 @@ mod fpscr {
 
             for bit_number in 0..32 {
                 bits[bit_number] = FpscrBit::new(
-                    bit_number as u8,
                     "Reserved",
                     Fpscr::is_bit_set(value, bit_number as u8),
                     true,
                 );
             }
 
-            bits[32] = FpscrBit::new(
-                32 as u8,
-                "FX",
-                Fpscr::is_bit_set(value, 32 as u8),
-                false,
-            );
-            bits[33] = FpscrBit::new(
-                32 as u8,
-                "FEX",
-                Fpscr::is_bit_set(value, 33 as u8),
-                false,
-            );
-            bits[34] = FpscrBit::new(
-                32 as u8,
-                "VX",
-                Fpscr::is_bit_set(value, 34 as u8),
-                false,
-            );
-            bits[35] = FpscrBit::new(
-                32 as u8,
-                "OX",
-                Fpscr::is_bit_set(value, 35 as u8),
-                false,
-            );
-            bits[36] = FpscrBit::new(
-                32 as u8,
-                "UX",
-                Fpscr::is_bit_set(value, 36 as u8),
-                false,
-            );
-            bits[37] = FpscrBit::new(
-                32 as u8,
-                "ZX",
-                Fpscr::is_bit_set(value, 37 as u8),
-                false,
-            );
-            bits[38] = FpscrBit::new(
-                32 as u8,
-                "XX",
-                Fpscr::is_bit_set(value, 38 as u8),
-                false,
-            );
+            bits[32] =
+                FpscrBit::new("FX", Fpscr::is_bit_set(value, 32 as u8), false);
+            bits[33] =
+                FpscrBit::new("FEX", Fpscr::is_bit_set(value, 33 as u8), false);
+            bits[34] =
+                FpscrBit::new("VX", Fpscr::is_bit_set(value, 34 as u8), false);
+            bits[35] =
+                FpscrBit::new("OX", Fpscr::is_bit_set(value, 35 as u8), false);
+            bits[36] =
+                FpscrBit::new("UX", Fpscr::is_bit_set(value, 36 as u8), false);
+            bits[37] =
+                FpscrBit::new("ZX", Fpscr::is_bit_set(value, 37 as u8), false);
+            bits[38] =
+                FpscrBit::new("XX", Fpscr::is_bit_set(value, 38 as u8), false);
             bits[39] = FpscrBit::new(
-                39 as u8,
                 "VXSNAN",
                 Fpscr::is_bit_set(value, 32 as u8),
                 false,
             );
             bits[40] = FpscrBit::new(
-                40 as u8,
                 "VXSISI",
                 Fpscr::is_bit_set(value, 32 as u8),
                 false,
             );
             bits[41] = FpscrBit::new(
-                32 as u8,
                 "VXIDI",
                 Fpscr::is_bit_set(value, 41 as u8),
                 false,
             );
             bits[42] = FpscrBit::new(
-                32 as u8,
                 "VXZDZ",
                 Fpscr::is_bit_set(value, 42 as u8),
                 false,
             );
             bits[43] = FpscrBit::new(
-                32 as u8,
                 "VXIMZ",
                 Fpscr::is_bit_set(value, 43 as u8),
                 false,
             );
             bits[44] = FpscrBit::new(
-                32 as u8,
                 "VXVC",
                 Fpscr::is_bit_set(value, 44 as u8),
                 false,
             );
-            bits[45] = FpscrBit::new(
-                32 as u8,
-                "FR",
-                Fpscr::is_bit_set(value, 45 as u8),
-                false,
-            );
-            bits[46] = FpscrBit::new(
-                32 as u8,
-                "FI",
-                Fpscr::is_bit_set(value, 46 as u8),
-                false,
-            );
+            bits[45] =
+                FpscrBit::new("FR", Fpscr::is_bit_set(value, 45 as u8), false);
+            bits[46] =
+                FpscrBit::new("FI", Fpscr::is_bit_set(value, 46 as u8), false);
             bits[47] = FpscrBit::new(
-                32 as u8,
                 "FPRF:C",
                 Fpscr::is_bit_set(value, 47 as u8),
                 false,
             );
             bits[48] = FpscrBit::new(
-                32 as u8,
                 "FPRF:FPCC:FL",
                 Fpscr::is_bit_set(value, 48 as u8),
                 false,
             );
             bits[49] = FpscrBit::new(
-                32 as u8,
                 "FPRF:FPCC:FG",
                 Fpscr::is_bit_set(value, 49 as u8),
                 false,
             );
             bits[50] = FpscrBit::new(
-                32 as u8,
                 "FPRF:FPCC:FE",
                 Fpscr::is_bit_set(value, 50 as u8),
                 false,
             );
             bits[51] = FpscrBit::new(
-                32 as u8,
                 "FPRF:FPCC:FU",
                 Fpscr::is_bit_set(value, 51 as u8),
                 false,
             );
             bits[52] = FpscrBit::new(
-                32 as u8,
                 "Reserved",
                 Fpscr::is_bit_set(value, 52 as u8),
                 true,
             );
             bits[53] = FpscrBit::new(
-                32 as u8,
                 "VXSOFT",
                 Fpscr::is_bit_set(value, 53 as u8),
                 false,
             );
             bits[54] = FpscrBit::new(
-                32 as u8,
                 "VXSQRT",
                 Fpscr::is_bit_set(value, 54 as u8),
                 false,
             );
             bits[55] = FpscrBit::new(
-                32 as u8,
                 "VXCVI",
                 Fpscr::is_bit_set(value, 55 as u8),
                 false,
             );
-            bits[56] = FpscrBit::new(
-                32 as u8,
-                "VE",
-                Fpscr::is_bit_set(value, 56 as u8),
-                false,
-            );
-            bits[57] = FpscrBit::new(
-                32 as u8,
-                "OE",
-                Fpscr::is_bit_set(value, 57 as u8),
-                false,
-            );
-            bits[58] = FpscrBit::new(
-                32 as u8,
-                "UE",
-                Fpscr::is_bit_set(value, 58 as u8),
-                false,
-            );
-            bits[59] = FpscrBit::new(
-                32 as u8,
-                "ZE",
-                Fpscr::is_bit_set(value, 59 as u8),
-                false,
-            );
-            bits[60] = FpscrBit::new(
-                32 as u8,
-                "XE",
-                Fpscr::is_bit_set(value, 60 as u8),
-                false,
-            );
-            bits[61] = FpscrBit::new(
-                32 as u8,
-                "NI",
-                Fpscr::is_bit_set(value, 61 as u8),
-                false,
-            );
-            bits[62] = FpscrBit::new(
-                32 as u8,
-                "RN0",
-                Fpscr::is_bit_set(value, 62 as u8),
-                false,
-            );
-            bits[63] = FpscrBit::new(
-                32 as u8,
-                "RN1",
-                Fpscr::is_bit_set(value, 63 as u8),
-                false,
-            );
+            bits[56] =
+                FpscrBit::new("VE", Fpscr::is_bit_set(value, 56 as u8), false);
+            bits[57] =
+                FpscrBit::new("OE", Fpscr::is_bit_set(value, 57 as u8), false);
+            bits[58] =
+                FpscrBit::new("UE", Fpscr::is_bit_set(value, 58 as u8), false);
+            bits[59] =
+                FpscrBit::new("ZE", Fpscr::is_bit_set(value, 59 as u8), false);
+            bits[60] =
+                FpscrBit::new("XE", Fpscr::is_bit_set(value, 60 as u8), false);
+            bits[61] =
+                FpscrBit::new("NI", Fpscr::is_bit_set(value, 61 as u8), false);
+            bits[62] =
+                FpscrBit::new("RN0", Fpscr::is_bit_set(value, 62 as u8), false);
+            bits[63] =
+                FpscrBit::new("RN1", Fpscr::is_bit_set(value, 63 as u8), false);
 
             Fpscr { bits }
         }
